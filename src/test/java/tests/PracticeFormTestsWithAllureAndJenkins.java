5966bc0a4615ceac5e8cdfc909ce52f896711f23
Artem_Bobkov <abobkov@dnomads.pro>
2022-07-04 18:12:54 +0700

    added Allure and Jenkins tests

---
+++ src/test/java/tests/PracticeFormTestsWithAllureAndJenkins.java
@@ -0,0 +1,84 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class PracticeFormTestsWithAllureAndJenkins extends TestBase {
+
+    @Test
+    void successfulTest() {
+        step("Open registration form", () -> {
+            open("/automation-practice-form");
+
+            executeJavaScript("$('footer').remove()");
+            executeJavaScript("$('#fixedban').remove()");
+        });
+
+        step("Fill textboxes", () -> {
+            $("#firstName").setValue("Artem");
+            $("#lastName").setValue("Bobkov");
+            $("#userEmail").setValue("abobkov@dnomads.pro");
+            $("#userNumber").setValue("1234567899");
+            $("#currentAddress").setValue("Moscow");
+        });
+
+        step("Fill Subject", () -> {
+            $("#subjectsInput").sendKeys("Physics");
+            $("#subjectsInput").pressEnter();
+        });
+
+        step("Fill date", () -> {
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("October");
+            $(".react-datepicker__year-select").selectOption("1997");
+            $(".react-datepicker__day--014").click();
+        });
+
+        step("Choose gender radio", () -> $("#genterWrapper").$(byText("Male")).click());
+
+        step("Mark checkboxes", () -> {
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            $("#hobbiesWrapper").$(byText("Reading")).click();
+            $("#hobbiesWrapper").$(byText("Music")).click();
+        });
+
+        step("Fill state and city", () -> {
+            $("#react-select-3-input").setValue("Rajasthan").pressEnter();
+            $("#react-select-4-input").setValue("Jaiselmer").pressEnter();
+        });
+
+        step("Upload image", () -> {
+            $("#uploadPicture").uploadFromClasspath("example.png");
+        });
+
+        step("Submit", () -> $("#submit").click());
+
+        step("Check form results", () -> {
+            $$(".table-responsive tr").filterBy(text("Student Name")).shouldHave(texts(
+                    "Artem Bobkov"));
+            $$(".table-responsive tr").filterBy(text("Student Email")).shouldHave(texts(
+                    "abobkov@dnomads.pro"));
+            $$(".table-responsive tr").filterBy(text("Gender")).shouldHave(texts(
+                    "Male"));
+            $$(".table-responsive tr").filterBy(text("Mobile")).shouldHave(texts(
+                    "1234567899"));
+            $$(".table-responsive tr").filterBy(text("Date of Birth")).shouldHave(texts(
+                    "14 October,1997"));
+            $$(".table-responsive tr").filterBy(text("Subjects")).shouldHave(texts(
+                    "Physics"));
+            $$(".table-responsive tr").filterBy(text("Hobbies")).shouldHave(texts(
+                    "Sports, Reading, Music"));
+            $$(".table-responsive tr").filterBy(text("Picture")).shouldHave(texts(
+                    "example.png"));
+            $$(".table-responsive tr").filterBy(text("Address")).shouldHave(texts(
+                    "Moscow"));
+            $$(".table-responsive tr").filterBy(text("State and City")).shouldHave(texts(
+                    "Rajasthan Jaiselmer"));
+        });
+    }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import pages.components.Attachments;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+    }
+}
